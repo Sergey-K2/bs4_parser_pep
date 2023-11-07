cdbce857720bb1b1d2ef5d10f97f9ada68d0edf8
Sergey Kozlov <serand.kozlov@gmail.com>
2023-11-08 02:15:27 +0300

    Second review

--- src/outputs.py
+++ src/outputs.py
@@ -5,10 +5,11 @@ import logging
 from prettytable import PrettyTable
 
 from constants import (
+    BASE_DIR,
     DATETIME_FORMAT,
     PRETTY_ARGUMENT,
     FILE_ARGUMENT,
-    RESULTS_DIR,
+    RESULTS_NAME,
     SAVING_LOGGING_PHRASE,
 )
 
@@ -27,11 +28,13 @@ def pretty_output(results):
 
 
 def file_output(results, cli_args):
-    RESULTS_DIR.mkdir(exist_ok=True)
+    # Не проходит pytest при использовании констант
+    results_dir = BASE_DIR / RESULTS_NAME
+    results_dir.mkdir(exist_ok=True)
     parser_mode = cli_args.mode
     now_formatted = dt.datetime.now().strftime(DATETIME_FORMAT)
     file_name = f"{parser_mode}_{now_formatted}.csv"
-    file_path = RESULTS_DIR / file_name
+    file_path = BASE_DIR / file_name
     with open(file_path, "w", encoding="utf-8") as f:
         writer = csv.writer(f, dialect=csv.unix_dialect)
         writer.writerows(results)
